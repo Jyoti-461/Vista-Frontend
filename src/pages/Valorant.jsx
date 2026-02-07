0930e2f223361f3212669b7f5715b1ae033d4e89
Jyoti-461 <jyotijayantofficial@gmail.com>
2026-02-07 10:28:12 +0530

    Update Valorant.jsx
    valo rules update

--- src/pages/Valorant.jsx
+++ src/pages/Valorant.jsx
@@ -59,7 +59,8 @@ useEffect(() => {
     </h2>
 
     <ul className="space-y-4 text-gray-400 text-sm md:text-base leading-relaxed list-disc list-inside">
-      <li>The Organisers will Provide <strong>75Hz Desktop.</strong>Bring your own <strong>Headphone</strong>or if required you can bring your own Gaming Mouse/Keyboard.</li>
+      <li>The Organisers will Provide <strong>75Hz Desktop.</strong>Bring your own <strong>Headphone</strong>.</li>
+<li>You can bring your own Mouse Pad or Gaming Mouse/Keyboard(if needed). </li>
       <li>Matches will be played in <strong>Custom Game</strong> mode.</li>
       <li>Game settings must be <strong>Standard</strong> with <strong>Tournament Mode ON</strong>.</li>
       <li>All matches will be conducted in a <strong>5v5 format</strong>.</li>
